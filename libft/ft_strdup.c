9c5e3a944ba574e50d130a7004f67c721dd94e22
SamueleGalli <80041753+SamueleGalli@users.noreply.github.com>
2023-11-11 16:47:29 +0100

    first commit

---
+++ libft/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgalli <sgalli@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 13:55:52 by sgalli            #+#    #+#             */
+/*   Updated: 2023/11/07 17:28:34 by sgalli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*new;
+	int		len;
+	int		i;
+
+	len = (int)ft_strrlen(s) + 1;
+	new = malloc(len);
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = 0;
+	return (new);
+}
---
+++ libft/ft_strjoin.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgalli <sgalli@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 12:15:40 by sgalli            #+#    #+#             */
+/*   Updated: 2023/11/07 19:02:59 by sgalli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*st;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+		return (s2);
+	else if (!s2)
+		return (s1);
+	else if (!s1 && !s2)
+		return (NULL);
+	st = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (st == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		st[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		st[i++] = s2[j++];
+	st[i] = 0;
+	return (st);
+}
+
+void	continue_join_n(char *st, char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		st[i] = s1[i];
+		i++;
+	}
+	st[i] = '\n';
+	while (s2[j])
+		st[i++] = s2[j++];
+	st[i] = '\n';
+	i++;
+	st[i] = 0;
+}
+
+char	*ft_strjoin_n(char *s1, char *s2)
+{
+	char	*st;
+
+	if (!s1 && !s2)
+		return (NULL);
+	else if (!s1)
+	{
+		st = (char *)malloc(sizeof(char) * (ft_strlen(s2) + 1 + 1));
+		st = ft_strcpy(st, s2);
+		st[ft_strlen(st) + 1] = '\0';
+		st[ft_strlen(st)] = '\n';
+		return (st);
+	}
+	st = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1 + 2));
+	if (st == NULL)
+		return (NULL);
+	continue_join_n(st, s1, s2);
+	return (st);
+}
